042fcf8dede22ffeba8b7a4f74524b3908f0793c
Abigail Davies <abz2489@gmail.com>
2023-04-22 21:31:40 +0000

    Add code to increment score and incorrect answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -49,8 +49,10 @@ function checkAnswer() {
 
     if (isCorrect) {
         alert('Hey! You got it right! :D');
+        incrementScore();
     } else {
         alert(`Awww.... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+        incrementWrongAnswer();
     }
 
     runGame(calculatedAnswer[1]);
@@ -73,12 +75,27 @@ function calculateCorrectAnswer() {
     }
 }
 
+/**
+ * Gets the current score from the DOM and increments it by 1.
+ */
 function incrementScore() {
+    let oldScore = parseInt(document.getElementById('score').innerText);
+    document.getElementById('score').innerText = ++oldScore;
+
+    if (userAnswer === calculatedAnswer[1]) {
+        score + 1;
+    } else {
+        score = score;
+    }
 
 }
 
+/**
+ * Gets the current tally of incorrect answers from the DOM and increments it by 1.
+ */
 function incrementWrongAnswer() {
-
+    let oldScore = parseInt(document.getElementById('incorrect').innerText);
+    document.getElementById('incorrect').innerText = ++oldScore;
 }
 
 function displayAdditionQuestion(operand1, operand2) {

666669d7a07e25a6f34eae9d8af2d7dc2ea3b86a
Abigail Davies <abz2489@gmail.com>
2023-04-22 05:18:31 +0000

    Set up game structure

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+//Step 1: Decide on functions needed and create
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+
+}
